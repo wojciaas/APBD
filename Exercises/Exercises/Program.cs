917b03dc215150b8438d1839b04c753a1ddf6127
s27994 <s27994@pjwstk.edu.pl>
2024-03-11 16:09:21 +0100

    Modyfikacja 1

--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        //camelCase - Java
+        //addStudent (Student stud) - Java
+        //PascalCase - C#
+        //AddStudent (Student stud) - C#
         Console.WriteLine("Hello, World!");
     }
 }
