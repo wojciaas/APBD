318183c1c215f19d6c53a563b5dfb9de4f4f66f9
s27994 <s27994@pjwstk.edu.pl>
2024-03-11 16:17:19 +0100

    Modyfikacja 3

--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -8,14 +8,27 @@ class Program
         //addStudent (Student stud) - Java
         //PascalCase - C#
         //AddStudent (Student stud) - C#
-        Console.WriteLine("Hello, World!");
+        int[] arr = { 1, 2, 7, 4 };
+        Console.WriteLine("Średnia: " + AvarageVal(arr));
+        Console.WriteLine("Max element: " + MaxElement(arr));
     }
 
-    int AvarageVal(int[] arr)
+    static int AvarageVal(int[] arr)
     {
         int sum = 0;
         foreach (int num in arr)
             sum += num;
         return sum / arr.Length;
+    }
+
+    static int MaxElement(int[] arr)
+    {
+        int max = arr[0];
+        foreach(int num in arr)
+        {
+            if (num > max)
+                max = num;
+        }
+        return max;
     }
 }
